45f347542dac4a6b26e8f91a45677639ad186854
DevFC-M <felipech3344@gmail.com>
2024-02-27 22:01:37 -0300

    Finalizando a estrutura de saida do projeto

---
+++ gerenciador.py
@@ -0,0 +1,15 @@
+while True:
+    print("\nMenu do Gerenciador de lista de tarefas:")
+    print("1. Adicionar tarefa")
+    print("2. Ver tarefa")
+    print("3. Atualizar tarefas")
+    print("4. Completar tarefa")
+    print("5. Deletar tarefa")
+    print("6. Sair")
+
+    escolha = input("Digite a opção desejada: ")
+
+    if escolha == "6":
+        break
+
+print("Programa finalizado")
