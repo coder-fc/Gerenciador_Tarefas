80f18691cc3abded47850bdb5424933b681f8c87
DevFC-M <felipech3344@gmail.com>
2024-02-28 21:57:43 -0300

    Adicionando a função de atualização de nome da tarefa

--- gerenciador.py
+++ gerenciador.py
@@ -14,6 +14,17 @@ def ver_tarefas(tarefas):
         status = "✓" if tarefa["completada"] else " "
         nome_tarefa = tarefa["tarefa"]
         print(f"{indice}. [{status}] {nome_tarefa}")
+    return
+
+
+def atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome):
+    indice_tarefa_ajustado = int(indice_tarefa) - 1
+    if indice_tarefa_ajustado >= 0 and indice_tarefa_ajustado < len(tarefas):
+        tarefas[indice_tarefa_ajustado]["tarefa"] = novo_nome
+        print(f"Tarefa {indice_tarefa} atualizada para {novo_nome}")
+    else:
+        print("Tarefa não é valida")
+    return
 
 
 tarefas = []
@@ -33,6 +44,12 @@ while True:
         adicionar_tarefa(tarefas, nome_tarefa)
     elif escolha == "2":
         ver_tarefas(tarefas)
+    elif escolha == "3":
+        ver_tarefas(tarefas)
+        indice_tarefa = input(
+            "Digite o número da tarefa que deseja atualizar: ")
+        novo_nome = input("Digite o novo nome da tarefa: ")
+        atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
     elif escolha == "6":
         break
 
